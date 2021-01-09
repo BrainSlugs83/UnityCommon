515e9e80a734f74013df033b868323ab027b0962
Mikey <BrainSlugs83@gmail.com>
2021-01-09 15:20:46 -0800

    Added Easing Utils

---
+++ Utils/LerpCounter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LerpCounter
+{
+    public float Start { get; set; }
+
+    private float _stop;
+
+    public float Stop
+    {
+        get { return _stop; }
+        set
+        {
+            if (_stop != value)
+            {
+                Start = Current;
+                _stop = value;
+                Counter = 0;
+            }
+        }
+    }
+
+    public float Counter { get; private set; }
+
+    public Func<float, float> EasingFunc { get; set; } = EasingUtils.LinearInterpolation;
+
+    public float Current => Mathf.Lerp(Start, Stop, EasingFunc(Counter));
+
+    public void Count(float amount)
+    {
+        Counter = Mathf.Clamp01(Counter + amount);
+    }
+}
---
+++ Utils/LerpCounter3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LerpCounter3
+{
+    public Vector3 Start { get; set; }
+
+    private Vector3 _stop;
+
+    public Vector3 Stop
+    {
+        get { return _stop; }
+        set
+        {
+            if (_stop != value)
+            {
+                Start = Current;
+                _stop = value;
+                Counter = 0;
+            }
+        }
+    }
+
+    public float Counter { get; private set; }
+
+    public Func<float, float> EasingFunc { get; set; } = EasingUtils.LinearInterpolation;
+
+    public Vector3 Current => Vector3.Lerp(Start, Stop, EasingFunc(Counter));
+
+    public void Count(float amount)
+    {
+        Counter = Mathf.Clamp01(Counter + amount);
+    }
+}
