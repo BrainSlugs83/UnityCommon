87219d4730fa4d74b191e3dc69d9e411ef87663a
Mikey <BrainSlugs83@gmail.com>
2021-06-27 18:13:59 -0700

    Made a bunch of additions.

---
+++ Common/Editor/InstallPackages.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class InstallPackages : EditorWindow
+{
+    private static readonly string AssetRootPath = Path.Combine
+    (
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "Unity"
+    );
+
+    [SerializeField]
+    private List<string> Packages = new List<string>(new[]
+    {
+        "DOTween HOTween v2",
+        "Grabbit - Editor Physics Transforms",
+        "VR Tunnelling Pro"
+    });
+
+    [SerializeField]
+    private Vector2 scrollPos = Vector2.zero;
+
+    [SerializeReference]
+    private Dictionary<string, string> FullPathLookups = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
+    [MenuItem("Tools/Install Packages")]
+    private static void CreateInstallPackagesWindow()
+    {
+        GetWindow<InstallPackages>();
+    }
+
+    private IEnumerable<string> SearchForPackages(string folder)
+    {
+        foreach (var x in Directory.GetFiles(folder))
+        {
+            if (Path.GetExtension(x).ToUpperInvariant() == ".UNITYPACKAGE")
+            {
+                var y = Path.GetFileNameWithoutExtension(x)?.Trim();
+                if (!string.IsNullOrWhiteSpace(y))
+                {
+                    FullPathLookups[y] = x;
+                    yield return y;
+                }
+            }
+        }
+
+        foreach (var d in Directory.GetDirectories(folder))
+        {
+            foreach (var pkg in SearchForPackages(d)) { yield return pkg; }
+        }
+    }
+
+    private void OnGUI()
+    {
+        var target = this as ScriptableObject;
+        var so = new SerializedObject(target);
+        var stringsProperty = so.FindProperty(nameof(Packages));
+
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        EditorGUILayout.PropertyField(stringsProperty, true);
+        EditorGUILayout.EndScrollView();
+
+        GUI.enabled = Packages.Any();
+        if (GUILayout.Button("Install"))
+        {
+            FullPathLookups.Clear();
+            SearchForPackages(AssetRootPath).ToArray();
+
+            foreach (var pkg in Packages)
+            {
+                if (string.IsNullOrWhiteSpace(pkg)) { continue; }
+                var pkgTrim = pkg.Trim();
+                if (!FullPathLookups.ContainsKey(pkgTrim))
+                {
+                    Debug.LogError($"Unable to find package: '{pkgTrim}'.");
+                }
+                else
+                {
+                    Debug.Log($"Installing Package: '{pkgTrim}' . . .");
+                    AssetDatabase.ImportPackage(FullPathLookups[pkg], false);
+                }
+            }
+
+            Close();
+        }
+        GUI.enabled = true;
+    }
+}
